052188af7c47ae615a17fe18fabe27564df9e13e
Karl Pfister <kpfister@me.com>
2021-08-19 12:03:25 -0600

    Inital Commit
    Commits intial project creation

---
+++ Meterologize/Meterologize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meterologize
+//
+//  Created by Karl Pfister on 8/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
